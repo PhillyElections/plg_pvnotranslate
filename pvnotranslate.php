ecd99d3debda3e815bf89e2c919bdef8fb5597b3
matthew murphy <mattyhead@gmail.com>
2019-01-14 09:08:22 -0500

    ...

--- pvnotranslate.php
+++ pvnotranslate.php
@@ -181,10 +181,10 @@ class plgContentPvnotranslate extends JPlugin
             //return true;
         }
         jimport("kint.kint");
-        $search = "(_\+Al Schmidt_\+|_\+Lisa Deeley_\+|_\+Anthony Clarke_\+|_\+Deeley_\+|_\+Schmidt_\+|_\+Clarke_\+)";
+        $search = "(_-Al Schmidt_-|_-Lisa Deeley_-|_-Anthony Clarke_-|_-Deeley_-|_-Schmidt_-|_-Clarke_-)";
 
         while (preg_match($search, $text, $regs, PREG_OFFSET_CAPTURE)) {
-            $string = JString::str_ireplace("_+","",$regs[0][0]);
+            $string = JString::str_ireplace("_-","",$regs[0][0]);
 //            d($regs, $search, $regs[0][0], "<span class=\"notranslate\">$string</div>", $text);
             //$string = $temp[1];
 
